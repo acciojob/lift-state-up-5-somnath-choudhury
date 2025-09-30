bf2635cc095e6527d7a86cb78578d814f1b827c2
somnath-choudhury <awesome8somnath@gmail.com>
2025-09-30 14:58:45 +0530

    project completed

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,28 @@
 
-import React from "react";
+import React, { useState } from "react";
 import './../styles/App.css';
+import LoginForm from "./LoginForm";
 
 const App = () => {
+
+  const [isLoggedIn, setIsLoggedIn] = useState(false)
+
+  const handleLogin = () => {
+    setIsLoggedIn(true)
+  }
+
   return (
     <div>
         {/* Do not remove the main div */}
+        <div style={{padding: '2rem', fontFamily:'Arial'}}>
+          <h1>Parent Component</h1>
+
+          {isLoggedIn ? (
+            <h2>You are logged in !</h2>
+          ) : (
+            <LoginForm onLogin={handleLogin} />
+          )}
+        </div>
     </div>
   )
 }
---
+++ src/components/LoginForm.js
@@ -0,0 +1,43 @@
+import React, {useState} from 'react'
+
+function LoginForm ({onLogin})  {
+  
+  const[username, setUsername] = useState('')
+  const[password, setPassword] = useState('')
+
+  const handleSubmit =  (e) => {
+    e.preventDefault()
+    onLogin();
+  }
+
+  return (
+    <form onSubmit={handleSubmit}>
+
+        <div style={{marginBottom:'1rem'}}>
+            <input
+            type='text'
+            placeholder='Username'
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
+            style={{padding: '0.5rem', width: '200px'}}
+            />
+        </div>
+        <div style={{marginBottom:'1rem'}}>
+            <input
+          type="password"
+          placeholder="Password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+          style={{ padding: '0.5rem', width: '200px' }}
+        />
+        </div>
+
+        <button type='submit' style={{padding: '0.5rem 1rem'}}>
+            Login
+        </button>
+
+    </form>
+  )
+}
+
+export default LoginForm
